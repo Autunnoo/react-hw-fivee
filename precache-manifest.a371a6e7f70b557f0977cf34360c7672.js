5bffa81b5d9414b42735bdfa7b9091c094864b35
Autunnoo <68438045+Autunnoo@users.noreply.github.com>
2020-09-23 16:19:40 +0300

    Updates

---
+++ precache-manifest.a371a6e7f70b557f0977cf34360c7672.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "480df6b72486be02135cf887931d32ac",
+    "url": "/react-hw-fivee/index.html"
+  },
+  {
+    "revision": "bd89d2724c71b19eb822",
+    "url": "/react-hw-fivee/static/css/main.55deca55.chunk.css"
+  },
+  {
+    "revision": "4b94a948d72659598187",
+    "url": "/react-hw-fivee/static/js/2.fde232f3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-hw-fivee/static/js/2.fde232f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd89d2724c71b19eb822",
+    "url": "/react-hw-fivee/static/js/main.8093d560.chunk.js"
+  },
+  {
+    "revision": "371ba4578c2e1951b39c",
+    "url": "/react-hw-fivee/static/js/runtime-main.41504894.js"
+  }
+]);
